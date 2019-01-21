a9b6d9584da2638ea36e6587c51aa63a94927a0d
sunnychan <sunnnychan@gmail.com>
2019-01-21 15:17:02 +0800

    common-infra

---
+++ rw-server/src/main/java/com/sunny/rw/server/domain/biz/BaseBiz.java
@@ -0,0 +1,9 @@
+package com.sunny.rw.server.domain.biz;
+
+/*
+ * Created by sunnnychan@outlook.com on 2019/1/21.
+ */
+
+public class BaseBiz {
+
+}
---
+++ rw-server/src/main/java/com/sunny/rw/server/domain/def/ServiceDef.java
@@ -0,0 +1,9 @@
+package com.sunny.rw.server.domain.def;
+
+/*
+ * Created by sunnnychan@outlook.com on 2019/1/21.
+ */
+
+public class ServiceDef {
+  public static String SERVICE_NAME = "";
+}

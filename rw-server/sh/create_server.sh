316c16ed7479086be5b7651956ee9fcd5fe21f4f
sunnychan <sunnnychan@gmail.com>
2019-03-05 16:17:01 +0800

    sunnychan

--- rw-server/sh/create_server.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SERVER_NAME=$1
-
-BASE_DIR="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd )/.."
-CONF_DIR="${BASE_DIR}/conf"
-SH_DIR="${BASE_DIR}/sh"
-SRC_MODULE_DIR="${BASE_DIR}/src/main/java/com/sunny/rw/server/modules"
-

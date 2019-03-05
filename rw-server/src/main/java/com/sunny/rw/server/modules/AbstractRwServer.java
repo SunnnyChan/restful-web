c08bf727cf63354326047fa9789e836b670bcdf8
sunnychan <sunnnychan@gmail.com>
2019-03-05 16:15:22 +0800

    sunnychan

---
+++ rw-server/src/main/java/com/sunny/rw/server/modules/AbstractRwServer.java
@@ -0,0 +1,28 @@
+package com.sunny.rw.server.modules;
+
+import com.sunny.commom.utils.module.AbstractModule;
+import com.sunny.commom.web.jersey.JerseyServer;
+import com.sunny.rw.server.conf.ServerConf;
+import org.eclipse.jetty.server.Server;
+
+/*
+ * Created by sunnnychan@outlook.com on 2019/1/22.
+ */
+public abstract class AbstractRwServer {
+
+  protected void startServer() throws Exception {
+    Server server = JerseyServer.create(ServerConf.getServerPort(), ServerConf.getControllerClassPath());
+    server.start();
+  }
+
+  protected void startModules() throws Exception {
+    if (!ServerConf.getModules().isEmpty()) {
+      for (String moduleClassName : ServerConf.getModules()) {
+        Class classType = Class.forName(ServerConf.getModulesClassPath() + "." + moduleClassName);
+        AbstractModule module = (AbstractModule) classType.newInstance();
+        module.start();
+      }
+    }
+  }
+}
+
--- rw-server/src/main/java/com/sunny/rw/server/modules/RwServer.java
+++ rw-server/src/main/java/com/sunny/rw/server/modules/RwServer.java
@@ -1,15 +1,11 @@
 package com.sunny.rw.server.modules;
 
-import com.sunny.commom.utils.module.AbstractModule;
-import com.sunny.commom.web.jersey.JerseyServer;
 import com.sunny.infra.conf.parser.ConfInit;
-import com.sunny.rw.server.conf.ServerConf;
-import org.eclipse.jetty.server.Server;
 
 /*
  * Created by sunnnychan@outlook.com on 2019/1/22.
  */
-public class RwServer {
+public class RwServer extends AbstractRwServer {
 
   public static void main(String[] args) throws Exception {
     parserConf(args);
@@ -18,27 +14,12 @@ public class RwServer {
     rwServer.startModules();
   }
 
-  private static void parserConf(String[] args) {
+  protected static void parserConf(String[] args) {
     if (args.length != 1) {
       System.out.println("Usage: <config file path>");
       System.exit(1);
     }
     ConfInit.init(args[0]);
-  }
-
-  private void startServer() throws Exception {
-    Server server = JerseyServer.create(ServerConf.getServerPort(), ServerConf.getControllerClassPath());
-    server.start();
-  }
-
-  private void startModules() throws Exception {
-    if (!ServerConf.getModules().isEmpty()) {
-      for (String moduleClassName : ServerConf.getModules()) {
-        Class classType = Class.forName(ServerConf.getModulesClassPath() + "." + moduleClassName);
-        AbstractModule module = (AbstractModule) classType.newInstance();
-        module.start();
-      }
-    }
   }
 }
 

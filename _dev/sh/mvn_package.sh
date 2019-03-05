8ed73af8a140f114f19312d85723c21c4929aae0
sunnychan <sunnnychan@gmail.com>
2019-03-05 10:26:07 +0800

    sunnychan

---
+++ _dev/sh/mvn_package.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+PROJECT_DIR="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd )/../.."
+
+cd ${PROJECT_DIR} && mvn archetype:generate -DgroupId=$1 -DartifactId=$2 \
+-DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+
